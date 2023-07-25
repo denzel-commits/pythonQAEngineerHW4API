3ed870e0f613cdc46e14e5a85c3dc893c98a8966
denzel-commits <dguchinsky@yahoo.com>
2023-07-25 22:28:03 +0600

    Refactor: change test name

--- tests/test_url.py
+++ tests/test_url.py
@@ -4,7 +4,7 @@ import os
 
 
 @pytest.mark.parametrize("url, status_code", [(os.getenv("url"), os.getenv("status_code"))])
-def test_with_options(url, status_code):
+def test_url_status(url, status_code):
     result = requests.get(url)
 
     assert result.status_code == int(status_code)
